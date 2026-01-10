db6bbd343c9528feaf82d17790fdc1458358d134
Darryl <darrylparker90@icloud.com>
2026-01-09 20:55:22 -0500

    document things

--- game.py
+++ game.py
@@ -3,22 +3,24 @@ import random
 
 NEW_GAME = 0
 
-def game_enter_number(self, prompt):
-    match_number = random.randint(1, 99)
-    print(f"{prompt} {match_number}")
-    
-    number = int(input())
-    while number != match_number:
-        print(f"Try typing {match_number}")
-        number = int(input())
-    print("Match, Unlocking gameplay!")
+STATS = ["Strength", "Dexterity", "Wisdom", "Intelligence", "Consitution"]
 
-def get_number(prompt, validator = None):
+
+#Using to check if index value is there, if not default value provided
+def get_index(list, index, default):
+    if index < len(list):
+        return list[index]
+    return default
+
+def is_valid(*args):
+    return True
+
+def get_number(prompt, validator = is_valid):
     while True:
         print(prompt)
         try:
             number = int(input())
-            if not validator or validator(number):
+            if validator(number):
                 return number
         except:
             pass
@@ -68,14 +70,23 @@ class Game:
     def start(self):
         self.campaign = self.choose_campaign()
         print(self.campaign["name"])
-        set_state_with_number(self.campaign, "player_count", "How many players are in your campaign? (1-4)", validate_campaign_player_count, True)
-        players = []
-        for i in range(self.campaign["player_count"]):
-            player = {}
-            player["Strength"] = random.randint(10, 21)
-            players.append(player)
-        self.campaign["players"] = players
+        set_state_with_number(self.campaign, "character_count", "How many characters are in your campaign? (1-4)", validate_campaign_player_count, True)
         self.save_state()
+        characters = self.campaign.get("characters", [])
+        self.campaign["characters"] = characters
+        for i in range(self.campaign["character_count"]):
+            character = get_index(characters, i, {})
+            if i >= len(characters):
+                characters.append(character)
+            self.pick_stats(character)
+      
+        
+        #Add while loop to stay in game?
+        # while True:
+
+
+
+        
 
     def choose_campaign(self):
         campaigns = self.state.get("campaigns", [])
@@ -93,22 +104,27 @@ class Game:
             return campaign
         return campaigns[option - 1]
 
-
+    def pick_stats(self, character):
+        set_state_with_line(character, "name", "What is your characters name?", True)
+        self.save_state()
+        character_name = character["name"]
+        stat_pool = 75
+        for stat in STATS:
+            already_allocated_stat = character.get(stat, 0)
+            stat_pool = stat_pool - already_allocated_stat
         
-        
 
-
-    @property    
-    def gender(self):
-        return self.state["gender"]
-
-
-
-    # def set_state_for_race(self, path, prompt, options, skip_if_has_value = False):
-    #     if skip_if_has_value and self.state[path]:
-    #         return
-    #     self.state[path] = self.
-
+        def validate_stat(stat):
+            return 0 <= stat and stat <= stat_pool
+        if stat_pool > 0:
+            print(f"Pick stats for {character_name}")
+            while stat_pool > 0:
+                for stat in STATS:
+                    value = get_number(f"Pick your {stat} ({stat_pool} remaining points)", validate_stat)
+                    already_allocated_stat = character.get(stat, 0)
+                    stat_pool = stat_pool - value
+                    character[stat] = value + already_allocated_stat
+                    self.save_state()
 
     def save_state(self):
         with open("state.json", "w") as f:
