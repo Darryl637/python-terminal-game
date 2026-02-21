582b289fb7ba9c64aa1103bd313ca5fcdb8ddb7d
Darryl <darrylparker90@icloud.com>
2026-02-21 08:55:46 -0500

    many errors fixed

--- game.py
+++ game.py
@@ -1,14 +1,52 @@
 import json
-from typing import TypeVar, List, Callable, Any
+from typing import TypeVar, List, Callable, Any, Literal
 from colorama import Fore, Style, init, Back
 import uuid
 import os
+from pydantic import BaseModel
 
 T = TypeVar("T")
 
 NEW_GAME = 0
 
 STATS = ["Strength", "Dexterity", "Wisdom", "Intelligence", "Constitution"]
+
+
+class Character(BaseModel):
+    name: str = ""
+    Strength: int = 0
+    Dexterity: int = 0
+    Wisdom: int = 0
+    Intelligence: int = 0
+    Constitution: int = 0
+
+
+class Campaign(BaseModel):
+    characters: List[Character] = []
+    name: str = ""
+    room_id: str = ""
+    character_count: int = 0
+
+
+class State(BaseModel):
+    campaigns: List[Campaign] = []
+    rooms: dict[
+        str, "Room"
+    ] = {}  # "Room" the quotes are saying to wait until file is loaded
+
+
+class Room(BaseModel):
+    name: str = ""
+    desc: str = ""
+    actions: dict[str, "Action"] = {}
+
+
+class GoToRoomAction(BaseModel):
+    action: Literal["gotoroom"] = "gotoroom"
+    room_id: str
+
+
+Action = GoToRoomAction
 
 
 def generate_id():
@@ -42,24 +80,24 @@ def get_number(prompt: str, validator: Callable[[int], bool] = is_valid) -> int:
         print("Try again")
 
 
-def set_state_with_line(
-    map: dict[str, Any], path: str, prompt: str, skip_if_has_value=False
-):
-    if skip_if_has_value and path in map:
+def set_state_with_line(instance: Any, path: str, prompt: str, skip_if_has_value=False):
+    value = getattr(instance, path)
+    if skip_if_has_value and value:
         return
-    map[path] = get_line(prompt)
+    setattr(instance, path, get_line(prompt))
 
 
 def set_state_with_number(
-    map: dict[str, Any],
+    instance: Any,
     path: str,
     prompt: str,
     validator: Callable[[int], bool],
     skip_if_has_value=False,
 ):
-    if skip_if_has_value and path in map:
+    value = getattr(instance, path)
+    if skip_if_has_value and value:
         return
-    map[path] = get_number(prompt, validator)
+    setattr(instance, path, get_number(prompt, validator))
 
 
 # possible not needed now
@@ -108,21 +146,13 @@ def validate_campaign_player_count(number: int) -> bool:
 GO_TO_ROOM = "gotoroom"
 
 
-def make_room(name, desc, actions):
-    return {"name": name, "actions": actions, "desc": desc}
-
-
-def go_to_room(room_id):
-    return {"action": GO_TO_ROOM, "room_id": room_id}
-
-
 CLONING_TUBE_ID = "vnum0"
 
 ROOMS = {
-    CLONING_TUBE_ID: make_room(
-        "Cloning tube",
-        "You are in a cloning tube\n",
-        {},
+    CLONING_TUBE_ID: Room(
+        name="Cloning tube",
+        desc="You are in a cloning tube\n",
+        actions={},
     ),
 }
 
@@ -135,16 +165,16 @@ class Game:
 
         rooms = self.rooms
 
-        if action["action"] == GO_TO_ROOM:
-            room_id = action["room_id"]
-            return rooms[room_id]["name"]
+        if isinstance(action, GoToRoomAction):
+            room_id = action.room_id
+            return rooms[room_id].name
         return ""
 
     # make rooms save to json upon making room
     def start(self):
-        self.rooms = self.state.get("rooms", ROOMS)
+        self.rooms = self.state.rooms or ROOMS
         self.campaign = self.choose_campaign()  # -> magic -> function
-        self.campaign["room_id"] = self.campaign.get("room_id", CLONING_TUBE_ID)
+        self.campaign.room_id = self.campaign.room_id or CLONING_TUBE_ID
         set_state_with_number(
             self.campaign,
             "character_count",
@@ -153,24 +183,22 @@ class Game:
             skip_if_has_value=True,
         )
         self.save_state()
-        characters = self.campaign.get("characters", [])
-        self.campaign["characters"] = characters
-        for i in range(self.campaign["character_count"]):
-            character = get_index(characters, i, {})
+        characters = self.campaign.characters or []
+        self.campaign.characters = characters
+        for i in range(self.campaign.character_count):
+            character = get_index(characters, i, Character())
             if i >= len(characters):
                 characters.append(character)
             self.pick_stats(character)
 
         while True:
-            room_id = self.campaign["room_id"]
+            room_id = self.campaign.room_id
             room = self.rooms[room_id]
             actions = self.get_actions()
             print(
-                f"({room['name']}) \n--------------------------------------------------{Fore.RESET} "
+                f"({room.name}) \n--------------------------------------------------{Fore.RESET} "
             )
-            print(
-                f"{room['desc']} \n-------------------------------------------------- "
-            )
+            print(f"{room.desc} \n-------------------------------------------------- ")
 
             action = get_choice(
                 "Obvious exits:             ",
@@ -181,58 +209,62 @@ class Game:
             os.system("cls")
             match action:
                 case i if isinstance(i, int):
-                    direction = list(room["actions"].keys())[action]
-                    match room["actions"][direction]:
-                        case a if a["action"] == (GO_TO_ROOM):
-                            room_id = a["room_id"]
+                    direction = list(room.actions.keys())[
+                        action
+                    ]  # direction stores string north west east ext
+                    match room.actions[direction]:
+                        case a if isinstance(a, GoToRoomAction):
+                            room_id = a.room_id
 
-                            self.campaign["room_id"] = room_id
+                            self.campaign.room_id = room_id
                         case _:
                             pass
                 case s if s.startswith("makeroom "):
                     command, direction, *room_name = s.split()
                     room_name = " ".join(room_name)
                     room_id = generate_id()
-                    room["actions"][direction] = go_to_room(room_id)
-                    self.rooms[room_id] = make_room(room_name, "", {})
-                    self.state["rooms"] = self.rooms
+                    room.actions[direction] = GoToRoomAction(room_id=room_id)
+                    self.rooms[room_id] = Room(name=room_name, desc="", actions={})
+                    self.state.rooms = self.rooms
                 case s if s.startswith("makeroomc "):
                     command, todirection, fromdirection, *room_name = s.split()
                     room_name = " ".join(room_name)
                     room_id = generate_id()
-                    room["actions"][todirection] = go_to_room(room_id)
+                    room.actions[todirection] = GoToRoomAction(room_id=room_id)
 
-                    self.rooms[room_id] = make_room(
-                        room_name,
-                        "",
-                        {fromdirection: go_to_room(self.campaign["room_id"])},
+                    self.rooms[room_id] = Room(
+                        name=room_name,
+                        desc="",
+                        actions={
+                            fromdirection: GoToRoomAction(room_id=self.campaign.room_id)
+                        },
                     )
-                    self.state["rooms"] = self.rooms
+                    self.state.rooms = self.rooms
                 case s if s.startswith("setroomname "):
                     command, *room_name = s.split()
                     room_name = " ".join(room_name)
-                    room["name"] = room_name
+                    room.name = room_name
                 case id if id.startswith("vin "):
                     print(room_id)
                 case s if s.startswith("makeexit "):
                     command, direction, room_id = s.split()
-                    room["actions"][direction] = go_to_room(room_id)
+                    room.actions[direction] = GoToRoomAction(room_id=room_id)
                 case d if d.startswith("roomdesc "):
                     print("Enter new room description")
-                    room["desc"] = input()
+                    room.desc = input()  # changed .desc from ["desc"]
                 case d if d.startswith("deleteroom "):
                     command, toremove = s.split()
                     del self.rooms[toremove]
                     for room in self.rooms.values():
-                        for action_key, action_value in dict(room["actions"]).items():
-                            if action_value["room_id"] == toremove:
-                                del room["actions"][action_key]
+                        for action_key, action_value in dict(room.actions).items():
+                            if action_value.room_id == toremove:
+                                del room.actions[action_key]
                 case s if s.startswith("score"):
                     for character in characters:
-                        print(f"-= Score for {character['name']} =-")
+                        print(f"-= Score for {character.name} =-")
                         output = []
                         for stat in STATS:
-                            output.append(f"{stat[0:3]}: {character[stat]}")
+                            output.append(f"{stat[0:3]}: {getattr(character, stat)}")
 
                         print(", ".join(output))
                         print()
@@ -243,37 +275,35 @@ class Game:
 
     def get_actions(self):
         actions = []
-        roomid = self.campaign["room_id"]
+        roomid = self.campaign.room_id
         room = self.rooms[roomid]
-        for key, value in room["actions"].items():
+        for key, value in room.actions.items():
             room_name = self.get_room_name(value)
             actions.append(f"{key} - {room_name}")
         return actions
 
     def choose_campaign(self):
-        campaigns = self.state.get("campaigns", [])
+        campaigns = self.state.campaigns or []
         options = ["New Game"]
         for campaign in campaigns:
-            options.append(campaign["name"])
+            options.append(campaign.name)
         option = get_choice("Choose your campaign: ", options, True)
         if option == NEW_GAME:
-            campaign = {
-                "name": "Campaign " + str(option + len(options)),
-            }
+            campaign = Campaign(name="Campaign " + str(option + len(options)))
             campaigns.append(campaign)
-            self.state["campaigns"] = campaigns
+            self.state.campaigns = campaigns
             self.save_state()
             return campaign
         return campaigns[option - 1]
 
-    def pick_stats(self, character: dict[str, Any]):
+    def pick_stats(self, character: Character):
         # When using self here does it mean the names in characters is the instance
         set_state_with_line(character, "name", "What is your characters name?", True)
         self.save_state()
-        character_name = character["name"]
+        character_name = character.name
         stat_pool = 75
         for stat in STATS:
-            already_allocated_stat = character.get(stat, 0)
+            already_allocated_stat = getattr(character, stat) or 0
             stat_pool = stat_pool - already_allocated_stat
 
         def validate_stat(stat: int):
@@ -287,20 +317,20 @@ class Game:
                         f"Pick your {stat} ({stat_pool} remaining points)",
                         validate_stat,
                     )
-                    already_allocated_stat = character.get(stat, 0)
+                    already_allocated_stat = getattr(character, stat) or 0
                     stat_pool = stat_pool - value
-                    character[stat] = value + already_allocated_stat
+                    setattr(character, stat, value + already_allocated_stat)
                     self.save_state()
 
     def save_state(self):
         with open("state.json", "w") as f:
-            contents = json.dumps(self.state, indent=2)
+            contents = self.state.model_dump_json(indent=2)
             f.write(contents)
 
     def load_state(self):
         try:
             with open("state.json", "r") as f:
                 contents = f.read()
-                self.state = json.loads(contents)
+                self.state = State.model_validate_json(contents)
         except:
-            self.state = {}
+            self.state = State()
